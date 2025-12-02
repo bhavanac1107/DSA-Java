58322df6b8faab1f5b9832d849cd33a324046210
bhavanac1107 <bhavanac1107@gmail.com>
2025-12-02 14:06:56 +0530

    Edited line13

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,15 +1,16 @@
 import java.util.*;
 class Arrays{
 public static void main(String args[]){
-    Scanner in=new Scanner(System.in);
+    Scanner sc=new Scanner(System.in);
     int arr[]=new int[5];
 
     for(int i=0;i<5;i++){
-     arr[i]=in.nextInt();
+     arr[i]=sc.nextInt();
     }
    
     for(int i=0;i<5;i++){
         System.out.println(arr[i]);
-    }
+        sc.close();
+      }
   }
 } 
