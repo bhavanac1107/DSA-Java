6f065e20d9f96ddcda740415b796eb54cf6a4f77
bhavanac1107 <bhavanac1107@gmail.com>
2025-12-02 12:23:05 +0530

    Add

--- src/SwapAndReverse.java
+++ src/SwapAndReverse.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+public class SwapAndReverse{
     public static void main(String[] args){
         int[] arr={3,4,5,6,7};
         System.out.println(Arrays.toString(arr));
